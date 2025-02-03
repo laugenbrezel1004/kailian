279eec1894f832788267c7ab386640d9967afbfe
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2025-02-03 18:53:27 +0100

    inital

---
+++ lib/api.c
@@ -0,0 +1,19 @@
+#include "api.h"
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <stdio.h>
+
+void getResponse(void) {
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:11434");
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+    }
+}
