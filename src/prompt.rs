c787b40ac6964febfa2a2ea1cbad000b7f7be519
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2025-04-03 19:53:00 +0200

    basic flags

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead, IsTerminal};
+use clap::{Arg, Command};
 use std::env;
 use std::process;
 pub fn read_stdin() -> String {
@@ -9,6 +10,32 @@ pub fn read_stdin() -> String {
         process::exit(1);
     }
 //auf flags achten!!!
+    let matches = Command::new("Mein CLI-Programm")
+        .version("1.0.0")
+        .author("Autor Name <email@example.com>")
+        .about("Ein Beispielprogramm")
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .action(clap::ArgAction::SetTrue)
+                .help("Aktiviert den verbose-Modus"),
+        )
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Pfad zur Konfigurationsdatei"),
+        )
+        .get_matches();
+
+    if matches.get_flag("verbose") {
+        println!("Verbose-Modus aktiviert.");
+    }
+
+    if let Some(config_path) = matches.get_one::<String>("config") {
+        println!("Konfigurationsdatei: {}", config_path);
+    }
     let mut prompt = String::new();
     for part in &argv[1..] {
         prompt.push_str(part);
