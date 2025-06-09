be5208467bf5a05c395a66bae088776220bddb00
laurenz <laurenzschmidt214@gmail.com>
2025-06-09 17:09:33 +0200

    cleaner output of currently running model

--- src/ai/get_running_model.rs
+++ src/ai/get_running_model.rs
@@ -1,6 +1,7 @@
-use std::io::{stdout, Write};
 use crate::envs::core::ConfigVariables;
 use curl::easy::Easy;
+use serde_json::Value;
+use std::io::{Write, stdout};
 
 pub fn running_model(env_variables: &ConfigVariables) -> Result<(), String> {
     let mut easy = Easy::new();
@@ -9,10 +10,14 @@ pub fn running_model(env_variables: &ConfigVariables) -> Result<(), String> {
     //make output cleaner
     easy.url(&url).map_err(|e| e.to_string())?;
     easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
+        let value: Value = serde_json::from_slice(data).unwrap();
+        let model_value = &value["models"][0]["model"];
+        println!("Model: {}", model_value);
+        //stdout().write_all(data).unwrap();
         Ok(data.len())
-    }).map_err(|e|e.to_string())?;
-    easy.perform().map_err(|e|e.to_string())?;
+    })
+    .map_err(|e| e.to_string())?;
+    easy.perform().map_err(|e| e.to_string())?;
     println!(); // to get rid of the "%" at the end
     Ok(())
 }
